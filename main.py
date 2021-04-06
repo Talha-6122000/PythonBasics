33b622d6e77f462ccaa95dcba251819cfff1e154
Talha <bsem-f18-206@superior.edu.pk>
2021-04-06 21:42:05 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+# Different types of Numbers in python
+# Python has different types of numeric values or literal
+# For example integers 2 ,-2 etc
+# Floating point numbers float 3.272 or 100.0
+# if we put a floating point with an integer it will be called floating point number for example 2.0,100.0
+# floating point number does not matter if it has 0 after 100 or whatever
+# Now let's discuss some basic arithematic operations
+a=1
+b=6
+c=a+b
+print(c) # Addition
+a=7
+b=3
+c=a-b
+print(c) # Substraction
+a=7
+b=3
+c=a*b
+print(c) # Multiplcation
+a=7
+b=4
+c=a/b
+print(c) # Division
+a=7
+b=4
+c=a//b
+print(c) #Floor Division
+# Here what floor division does is it ignores the decimal part or in other words it trucates it without rounding
+# So what would be the modulo let's see it actually give us the reminder
+a=7
+b=4
+c=a%b
+print(c) #Reminder
+#Now how to take powers in python you just can use the multiplication operator twice to do that
+print(2**3)
+# Order of operations followed in python
+print(2+10*10+3) #it first multiply than add other values
+# You can use paranthesis to specify orders like
+print((2+10)*(10+3)) #Now the answer would be different
+# Let's talk about variables we sometimes call them objects as they are built in classes that we create objects of
+# We will learn more about it you just have to stay with me
+# We can assign different values to the variable
+# Let's do it then
+a=10
+# Let's print it using the function print
+print(a)
+# you can also do some basic arithematic operation on these variables
+# Let's do that too
+a=a+a
+print(a)
+
+
+
